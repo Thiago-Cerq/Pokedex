6822f3ccc3ce32044d0154381eb7e037c7559f73
Thiago <thiagocerq21@gmail.com>
2022-07-25 13:02:43 -0300

    Implementando JS fixed

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const getPokemonUrl = id = `httpspokeapi.coapiv2pokemon${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-const generatePokemonPromises = () = Array(150).fill().map((_,index) =
-    fetch(getPokemonUrl(index + 1)).then(response = response.json()))
+const generatePokemonPromises = () => Array(150).fill().map((_,index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const generateHTML = pokemons = pokemons.reduce((accumulator, {name, id ,types}) = {
-    const elemenTypes = types.map(typeInfo = typeInfo.type.name)
+const generateHTML = pokemons => pokemons.reduce((accumulator, {name, id ,types}) => {
+    const elemenTypes = types.map(typeInfo => typeInfo.type.name)
 
     accumulator += `
-        li class = card ${elemenTypes[0]}
-            img class=card-image alt=${name} src=httpsraw.githubusercontent.comPokeAPIspritesmasterspritespokemonotherdream-world${id}.svg  
-            h2 class = card-title${id}. ${name}h2
-            p class =card-subtitle${elemenTypes.join('  ')}p
-        li
+        <li class = "card ${elemenTypes[0]}">
+            <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" /> 
+            <h2 class = "card-title">${id}. ${name}</h2>
+            <p class ="card-subtitle">${elemenTypes.join(' | ')}</p>
+        </li>
         `
     return accumulator
 }, '')
 
-const insetPokemonsIntoPage = pokemons = {
-    const ul = document.querySelector('[data-js=pokedex]')
+const insetPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
     ul.innerHTML = pokemons
 }
 
